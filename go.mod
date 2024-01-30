ef38b45169095fe12ed1a1251a91b9ba2aab28e3
odmrs <omarcosviniciusdev@gmail.com>
2024-01-30 04:26:14 -0300

    add: testing connect in db

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/odmrs/crud-phonebook
 
 go 1.21.6
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	"github.com/odmrs/crud-phonebook/cmd/ui"
+	// Generic interface around SQL in general
+
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+	_ "github.com/odmrs/crud-phonebook/cmd/ui"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "example"
+	dbname   = "postgres"
 )
 
 func main() {
-	ui.DrawMenu()
+	//	ui.DrawMenu()
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	CheckError(err)
+
+	// Closing DB
+
+	defer db.Close()
+
+	// checking db
+
+	err = db.Ping()
+	CheckError(err)
+
+	fmt.Println("Connected!")
+}
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
