831d0277910b0aa47b926de3db12cffcd407ec9e
odmrs <omarcosviniciusdev@gmail.com>
2024-01-30 05:03:29 -0300

    add: clear the main function to add general logic

--- main.go
+++ main.go
@@ -3,62 +3,9 @@ package main
 import (
 	// Generic interface around SQL in general
 
-	"database/sql"
-	"fmt"
-
-	_ "github.com/lib/pq"
-	_ "github.com/odmrs/crud-phonebook/cmd/ui"
-)
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "example"
-	dbname   = "postgres"
+	"github.com/odmrs/crud-phonebook/cmd/ui"
 )
 
 func main() {
-	//	ui.DrawMenu()
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-
-	db, err := sql.Open("postgres", psqlconn)
-	CheckError(err)
-
-	// Closing DB
-
-	defer db.Close()
-
-	// Conection
-	e := db.Ping()
-	CheckError(e)
-	fmt.Println("Created!!")
-
-	// Create table
-
-	createTableQuery := `
-	CREATE TABLE IF NOT EXISTS contacts (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	phone TEXT
-	)
-`
-	_, err = db.Exec(createTableQuery)
-	CheckError(err)
-	fmt.Println("Table Created")
-
-	// Insert data
-
-	insetTest := `insert into "contacts"("name", "phone") values('Marcos', '321313')`
-
-	_, err = db.Exec(insetTest)
-	CheckError(err)
-
-	fmt.Println("Insert data!")
-}
-
-func CheckError(err error) {
-	if err != nil {
-		panic(err)
-	}
+	ui.DrawMenu()
 }
